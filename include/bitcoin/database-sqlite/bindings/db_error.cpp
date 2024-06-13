41c36350a6351391599f3afe41fa4ba2bc6b7093
DreadPirate07 <tanayraikhere@gmail.com>
2024-06-13 19:03:08 +0530

    refactored repository structure- rmdir binding/

--- include/bitcoin/database-sqlite/bindings/db_error.cpp
+++
@@ -1,8 +0,0 @@
-#include "db.hpp"
-#include "db_error.hpp"
-
-
-namespace db {
-    db_error::db_error(char const* msg) : std::runtime_error(msg) {}
-    db_error::db_error(database& db) : std::runtime_error(sqlite3_errmsg(db.db_)) {}
-}
